508f7d53bea1165bca5cf793347b43d1295a0fcf
dgahn <dgahn@si-analytics.ai>
2021-08-13 21:35:01 +0900

    fix:Build 파일 삭제

--- protobuf/build/scripts/protoc-gen-grpc-kotlin-1.0.0-jdk7-generateProto-trampoline.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec '/Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home/bin/java' -jar '/Users/dgahn/.gradle/caches/modules-2/files-2.1/io.grpc/protoc-gen-grpc-kotlin/1.0.0/d6c8cbe29c60b202165593be5244ce43d37378ac/protoc-gen-grpc-kotlin-1.0.0-jdk7.jar' "$@"
