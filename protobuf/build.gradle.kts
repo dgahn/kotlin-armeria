0da24e4e99b8a39f5eb3fc8def75014891fa6ee0
dgahn <dgahn@si-analytics.ai>
2021-08-13 21:44:44 +0900

    docs: 프로토버퍼 버전 변경

--- protobuf/build.gradle.kts
+++ protobuf/build.gradle.kts
@@ -11,20 +11,20 @@ configurations.forEach {
 dependencies {
     compileOnly("javax.annotation:javax.annotation-api:1.3.2")
 
-    api("com.google.protobuf:protobuf-java-util:3.14.0")
+    api("com.google.protobuf:protobuf-java-util:3.17.0")
     api("io.grpc:grpc-kotlin-stub:1.0.0")
-    api("io.grpc:grpc-protobuf:1.34.0")
-    api("io.grpc:grpc-netty-shaded:1.34.0")
+    api("io.grpc:grpc-protobuf:1.39.0")
+    api("io.grpc:grpc-netty-shaded:1.39.0")
 }
 
 protobuf {
     generatedFilesBaseDir = "$projectDir/build/generated/source"
     protoc {
-        artifact = "com.google.protobuf:protoc:3.14.0"
+        artifact = "com.google.protobuf:protoc:3.17.0"
     }
     plugins {
         id("grpc") {
-            artifact = "io.grpc:protoc-gen-grpc-java:1.34.0"
+            artifact = "io.grpc:protoc-gen-grpc-java:1.39.0"
         }
         id("grpckt") {
             artifact = "io.grpc:protoc-gen-grpc-kotlin:1.0.0:jdk7@jar"
